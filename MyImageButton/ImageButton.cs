f98b1a88102c2ce727fbf0a34b3d8f2fb3085d81
Angelo Luis <angelobelchior@hotmail.com>
2015-07-09 12:48:22 -0300

    Commit Inicial

---
+++ MyImageButton/ImageButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace MyImageButton
+{
+	public class ImageButton : Image
+	{
+		public static BindableProperty CommandProperty = 
+			BindableProperty.Create<ImageButton, ICommand> (bp => bp.Command, default(ICommand));
+
+		public ICommand Command {
+			get{ return (ICommand)this.GetValue (CommandProperty); }
+			set{ this.SetValue (CommandProperty, value); }
+		}
+
+		public ImageButton ()
+		{
+			var gesto = new TapGestureRecognizer ();
+			gesto.Tapped += (sender, e) => {
+				if(this.Command != null && this.Command.CanExecute(null))
+				{
+					this.Command.Execute(null);
+				}
+			};
+
+			this.GestureRecognizers.Add (gesto);
+		}
+	}
+}
---
+++ MyImageButton/ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MyImageButton
+{
+	public class ViewModel : INotifyPropertyChanged
+	{
+		#region INotifyPropertyChanged implementation
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion
+
+		string texto;
+		public string Texto {
+			get {
+				return texto;
+			}
+			set {
+				texto = value;this.Notify ("Texto");
+			}
+		}
+
+		public ICommand ToqueCommand {
+			get;
+			set;
+		}
+
+		public ViewModel ()
+		{
+			this.ToqueCommand = new Command (this.Toque);
+		}
+
+		private int i = 0;
+		private void Toque(object parameter)
+		{
+			this.Texto = string.Format ("Toque número {0}", i++);
+		}
+
+		private void Notify(string parameter)
+		{
+			if (this.PropertyChanged != null) {
+				this.PropertyChanged (this, new PropertyChangedEventArgs(parameter));
+			}
+		}
+	}
+}
+
